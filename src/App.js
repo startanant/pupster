7bcf62582ae4bb7cc6f6c0198fd4b8b785ebbc5b
Anant S <startanant@gmail.com>
2020-03-27 11:48:16 -0400

    Adds working Navbar:active

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NavBar from './Components/NavBar'
+import AboutPage from './Components/AboutPage'
+import DiscoverPage from './Components/DiscoverPage'
+import SearchPage from './Components/SearchPage'
 
 
 function App() {
+  const [ activePage, setActivePage ] = useState( 'AboutPage' );
+  
+  function changePage( page ) {
+    setActivePage ( page );
+  }
+  
+  
+  
   return (
     <div className="App">
-      
+      <NavBar activePage = {activePage} changePage={changePage}/>
+      { activePage==='AboutPage' ? <AboutPage/> : ''}
+      { activePage==='DiscoverPage' ? <DiscoverPage/> : ''}
+      { activePage==='SearchPage' ? <SearchPage/> : ''}
     </div>
   );
 }
---
+++ src/Components/DiscoverPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function DiscoverPage() {
+  return (
+    <div >
+      [DiscoverPage]
+    </div>
+  );
+}
+
+export default DiscoverPage;
